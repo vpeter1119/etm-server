c18d2e5b06a13ac200976aedbe36243abdfa41de
vpeter1119 <vpeter1119@gmail.com>
2019-09-16 21:06:54 +0200

    Added GET request to "admin/users" route to fetch all users.

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -31,4 +31,35 @@ router.get("/characters", checkAuth, (req, res, next) => {
   }
 });
 
+router.get("users", checkAuth, (req,res,next) => {
+	var origin = req.headers.origin;
+	var currentUser = req.userData.userId;
+	console.log("Users (admin) requested by: " + origin);
+	if (currentUser === process.env.ADMIN_ID) {
+		User.find({}, (err, allUsers) => {
+			if (err) {
+				console.log(err);
+				console.log("Failed to retrieve all characters.");
+				res.status(500).json({
+					message: "Server failed to retrieve users."
+				});
+			} else {
+				if (!allUsers) {
+					console.log("No users found.");
+					res.status(404).json({
+						message: "No users found."
+					});
+				} else {
+					console.log("Success.");
+					res.status(200).json(allCharacters);
+				}
+			}
+		}
+	} else {
+		res.status(401).json({
+			message: "Insufficicent permissions."
+		});
+	}
+});
+
 module.exports = router;
