bcb72c44a0aa509d986670016d44c02554ef29d5
vpeter1119 <48699290+vpeter1119@users.noreply.github.com>
2019-09-18 13:39:02 +0200

    Added article.js model via upload.

---
+++ models/article.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const articleSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+}, timestamps);
+
+const Article = mongoose.model("Article", articleSchema);
+
+module.exports = Article;
