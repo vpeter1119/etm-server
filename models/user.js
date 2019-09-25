706e0898b6d604cb3b3922398252e4ada9f37d1b
vpeter1119 <48699290+vpeter1119@users.noreply.github.com>
2019-09-25 11:50:36 +0200

    Added timestamps to users mongoose schema.

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
-});
+}, { timestamps: true });
 
 userSchema.plugin(uniqueValidator);
 
